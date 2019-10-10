f5c1154f955d07c6cbf1609a8594bf923b820f98
Reid Crawford <reidcrawford55@gmail.com>
2019-10-10 13:35:24 -0700

    created Explore Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './components/Header'
+import Explore from './components/Explore'
 import './App.css';
 import { Button } from 'reactstrap';
 
 function App() {
-  return (
-    <Header />
-  );
+  return [
+    <Header />,
+    <Explore />
+  ];
 }
 
 export default App;
---
+++ src/components/Explore.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Explore = () => {
+    return (
+        <div className="explore-airbnb">
+            <h3>Explore Airbnb</h3>
+            <ul className="explore-ul">
+                <li>
+                    <a href="#">
+                        <div className="explore-column">
+                            <div className="div-left-one"></div>
+                            <div className="div-right">
+                                <span>Stays</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="explore-column">
+                            <div className="div-left-two"></div>
+                            <div className="div-right">
+                                <span>Experiences</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="explore-column">
+                            <div className="div-left-three"></div>
+                            <div className="div-right">
+                                <span>Adventures</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="explore-column">
+                            <div className="div-left-four"></div>
+                            <div className="div-right">
+                                <span>Restaraunts</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Explore; 
